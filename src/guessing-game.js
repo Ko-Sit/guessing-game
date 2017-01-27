5d2a097dc0eb3b6462af0a9342b1f3424dd6138e
Konstantin Sitkin(450501) <upsitkink@gmail.com>
2017-01-28 02:36:49 +0300

    Add files via upload
    100% passing

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null
+        this.max = null
+        this.array = []
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        return Math.ceil((this.min + this.max) / 2)
     }
 
     lower() {
-
+        this.max = Math.ceil((this.min + this.max) / 2)
     }
 
     greater() {
-
+        this.min = Math.ceil((this.min + this.max) / 2)
     }
 }
 
